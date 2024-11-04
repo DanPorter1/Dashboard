32e4ac6f684b14a7e6044e92f7f244f5e7a9bb9d
DanPorter1 <158497242+DanPorter1@users.noreply.github.com>
2024-11-04 14:03:56 +0000

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,24 +6,16 @@ st.title("Dashboard")
 
 with st.sidebar:
     st.write("Testing")
-    add_selectbox = st.sidebar.selectbox(
+    inc_selectbox = st.sidebar.selectbox(
         "Select Incident type",
         ("All", "Pharmacy Manager", "HSCN", "EPS", "Other")
     )
-    add_radio = st.radio(
+    status_radio = st.radio(
         "Choose ticket status",
         ("Open", "On Hold", "Closed", "Resolved")
     )
 
 df = pd.read_csv("SampleData.csv")
 st.dataframe(df)
-if 'selectbox_changed' not in st.session_state:
-    st.session_state.selectbox_changed = False
 
-if add_selectbox != st.session_state.selected_incident_type:
-    st.session_state.selected_incident_type = add_selectbox
-    st.session_state.selectbox_changed = True
-
-if st.session_state.selectbox_changed:
-    filtered_df = df[df['Incident Summary'].str.contains(add_selectbox)]
-    st.dataframe(filtered_df)
+df.loc[(df['Incident Summary']=inc_selectbox)]
