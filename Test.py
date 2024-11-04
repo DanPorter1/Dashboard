789502f245f9abf8b950837123c1fef97e231589
DanPorter1 <158497242+DanPorter1@users.noreply.github.com>
2024-11-04 14:20:14 +0000

    Update Test.py

--- Test.py
+++ Test.py
@@ -17,8 +17,14 @@ with st.sidebar:
 
 df = pd.read_csv("SampleData.csv")
 
+if inc_selectbox == "All":
+    filtered_df = df
+elif inc_selectbox == "All" and status_radio != "All"
+    filtered_df =  = filtered_df[filtered_df['State'] == status_radio]
+else:
+    filtered_df = df[(df['Incident Summary'] == inc_selectbox) & (df['State'] == status_radio)]
+    
+    if status_radio != "All":
+        filtered_df = filtered_df[filtered_df['State'] == status_radio]
 
-st.write(filtered_df)
-filtered_df = df[df['Incident Summary'] == inc_selectbox] if inc_selectbox != "All" else df
-filtered_df = filtered_df[filtered_df['State'] == status_radio] if status_radio != "All" else filtered_df
 st.write(filtered_df)
