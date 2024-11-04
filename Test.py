3ef4d226e4172ae757041b12f1da9027ed11d2dc
DanPorter1 <158497242+DanPorter1@users.noreply.github.com>
2024-11-04 15:11:28 +0000

    Update Test.py

--- Test.py
+++ Test.py
@@ -30,19 +30,3 @@ else:
         filtered_df = filtered_df[filtered_df['State'] == status_radio]
 
 st.write(filtered_df)
-
-
-grouped_df = filtered_df.groupby('State').size().reset_index(name='Count')
-filtered_df['Open Date Short'] = filtered_df['Open Date'].str[:11]
-grouped_open_date = filtered_df.groupby('Open Date Short')['Open Date'].nunique().reset_index(name='Count')
-grouped_total = filtered_df.groupby(['State', 'Open Date Short']).size().reset_index(name='Count')
-
-chart1 = alt.Chart(grouped_df).encode(x='State', y='Count')
-
-chart2 = alt.Chart(grouped_open_date).encode(x='Open Date Short', y='Count')
-
-chart3 = alt.Chart(grouped_total).encode(x=alt.X('Open Date Short', sort=alt.EncodingSortField(field='Count', order='descending')), y='Count', color='State', column='State')
-
-combined_chart = chart1 | chart2 | chart3
-
-st.altair_chart(combined_chart, use_container_width=True)
