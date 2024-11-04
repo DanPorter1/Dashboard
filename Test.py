38c9e80f4513893c970eb00040b796ed3a65e9a9
DanPorter1 <158497242+DanPorter1@users.noreply.github.com>
2024-11-04 16:04:15 +0000

    Update Test.py

--- Test.py
+++ Test.py
@@ -39,9 +39,11 @@ if inc_selectbox == "All":
         filtered_df = df[df['State'] == status_radio]
     else: 
         filtered_df = df
+        st.write(filtered_df)
         barchart()
 else:
     filtered_df = df[(df['Incident Summary'] == inc_selectbox) & (df['State'] == status_radio)]
     
     if status_radio != "All":
         filtered_df = filtered_df[filtered_df['State'] == status_radio]
+st.write(filtered_df)
