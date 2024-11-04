4be12b754b1673553bd1eebaca7514d7d6fd3a88
DanPorter1 <158497242+DanPorter1@users.noreply.github.com>
2024-11-04 16:01:05 +0000

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,6 +5,20 @@ import altair as alt
 import plotly.express as px
 
 st.title("Dashboard")
+#==============================================================#
+def barchart():
+    df['Unique Date'] = df['Open Date'].str[:11]
+    # Group by 'Unique Date' and 'State' to get total incidents for each state on each unique date
+    grouped_df = filtered_df.groupby(['Unique Date', 'State']).size().unstack(fill_value=0).reset_index()
+    
+    # Melt the dataframe to have states as a single column
+    melted_df = pd.melt(grouped_df, id_vars=['Unique Date'], var_name='State', value_name='Total Incidents')
+    
+    # Plot grouped bar chart with adjusted bargap to make bars touch
+    fig = px.bar(melted_df, x='Unique Date', y='Total Incidents', color='State', barmode='group', labels={'value': 'Total Incidents', 'Unique Date': 'Date'}).update_traces(marker=dict(line=dict(width=0)))
+    
+    st.plotly_chart(fig)
+#==============================================================#
 
 with st.sidebar:
     st.write("Testing")
@@ -32,16 +46,3 @@ else:
         filtered_df = filtered_df[filtered_df['State'] == status_radio]
 
 st.write(filtered_df)
-#==============================================================#
-def barchart():
-    df['Unique Date'] = df['Open Date'].str[:11]
-    # Group by 'Unique Date' and 'State' to get total incidents for each state on each unique date
-    grouped_df = filtered_df.groupby(['Unique Date', 'State']).size().unstack(fill_value=0).reset_index()
-    
-    # Melt the dataframe to have states as a single column
-    melted_df = pd.melt(grouped_df, id_vars=['Unique Date'], var_name='State', value_name='Total Incidents')
-    
-    # Plot grouped bar chart with adjusted bargap to make bars touch
-    fig = px.bar(melted_df, x='Unique Date', y='Total Incidents', color='State', barmode='group', labels={'value': 'Total Incidents', 'Unique Date': 'Date'}).update_traces(marker=dict(line=dict(width=0)))
-    
-    st.plotly_chart(fig)
